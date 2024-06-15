e56273238c67bffc961d4e02395f9775e15a28ef
Ilman-Nurahman <ilman.xrpl2@gmail.com>
2024-06-15 19:06:10 +0700

    update db name

--- config/connection.php
+++ config/connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "management_stok";
+$dbname = "stokbarang";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
